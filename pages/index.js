98316251189eca64c44bf45df5c3d1e35d8971ca
Chris Coyier <chriscoyier@gmail.com>
2021-06-25 08:06:48 -0700

    options

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 import CodeEditor from "../components/CodeEditor";
 
 export default function Home() {
@@ -12,10 +12,94 @@ export default function Home() {
       <main className={styles.main}>
         <h1>CodeMirror 6 Needs</h1>
 
-        <div>
-          <select>
-            <option value=""></option>
-          </select>
+        <div className={styles.options}>
+          <div>
+            <label htmlFor="indent-width">Indent Width</label>
+            <select id="indent-width">
+              <option>2</option>
+              <option>4</option>
+              <option>6</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="syntax-highlighting">Syntax Highlighting</label>
+            <select id="syntax-highlighting">
+              <optgroup label="Dark Themes">
+                <option>Twilight</option>
+                <option>Solarized Dark</option>
+                <option>Tomorrow Night</option>
+                <option>Oceanic Dark</option>
+                <option>Panda DuoTone</option>
+                <option>Dark High</option>
+                <option>Contrast Dark</option>
+              </optgroup>
+              <optgroup label="Light Themes">
+                <option>Classic</option>
+                <option>Solarized Light</option>
+                <option>XQ Light</option>
+                <option>Oceanic Light</option>
+                <option>MDN Like</option>
+                <option>DuoTone Light</option>
+                <option>High Contrast Light</option>
+              </optgroup>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="font">Font</label>
+            <select id="font">
+              <option>Source Code Pro</option>
+              <option>Courier Prime</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="font-size">Font Size</label>
+            <select id="font-size">
+              <option>16px</option>
+              <option>20px</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="emmet">Emmet</label>
+            <select id="emmet">
+              <option>On</option>
+              <option>Off</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="line-numbers">Line Numbers</label>
+            <select id="line-numbers">
+              <option>On</option>
+              <option>Off</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="line-wrapping">Line Wrapping</label>
+            <select id="line-wrapping">
+              <option>On</option>
+              <option>Off</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="code-folding">Code Folding</label>
+            <select id="code-folding">
+              <option>On</option>
+              <option>Off</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="match-brackets">Match Brackets/Tags</label>
+            <select id="match-brackets">
+              <option>On</option>
+              <option>Off</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="autocomplete">Autocomplete</label>
+            <select id="autocomplete">
+              <option>On</option>
+              <option>Off</option>
+            </select>
+          </div>
         </div>
 
         <div className={styles.codeEditors}>
