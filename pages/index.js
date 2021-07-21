0e4875a6b00668868b398942b120645800754163
Chris Coyier <chriscoyier@gmail.com>
2021-07-21 06:40:35 -0700

    This is how Next.js wants custom fonts done

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
 import { useState } from "react";
 import Head from "next/head";
-
-import CodeEditor from "../components/CodeEditor";
 import CodeSamples from "../components/CodeSamples";
 import EditorSettings from "../components/EditorSettings";
-
 import { EDITOR_SETTINGS_DEFAULTS } from "../data/editorSettings";
-import { CODE_SAMPLES } from "../data/code";
-
 import styles from "../styles/Home.module.scss";
 
 export default function Home() {
@@ -20,12 +15,6 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>CodeMirror 6 Needs</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <main className={styles.main}>
