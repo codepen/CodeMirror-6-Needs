14d555d73319e18c7c4fc1fc29c56328b2a7fbe0
Stephen Shaw <shshaw@gmail.com>
2021-08-02 12:57:13 -0500

    Bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   target: "serverless",
-};
+});
